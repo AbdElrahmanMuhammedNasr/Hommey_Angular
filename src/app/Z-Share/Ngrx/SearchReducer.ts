61026156f298a301279198b3b04dafeaaaea8b41
AbdElrahmanMuhammedNasr <abdo300nasr@gmail.com>
2020-07-21 14:52:10 +0200

    init

---
+++ src/app/Z-Share/Ngrx/SearchReducer.ts
@@ -0,0 +1,19 @@
+import { Action } from '@ngrx/store';
+
+const initalState = {
+    Search_Word: ''
+};
+export function searchProduct(state = initalState, action: Action) {
+    if (action.type.length <= 0) {
+        return {
+            ...state,
+            Search_Word: ''
+        }
+    } else {
+        return {
+            ...state,
+            Search_Word: action.type
+        }
+
+    }
+}
